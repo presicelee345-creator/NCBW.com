a1788ca4852f1cca8dd22f28115f808fbb554995
Leepy7 <presice.lee345@gmail.com>
2026-02-25 14:50:03 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 export default defineConfig({
-  base: './',
+base: '/NCBW.com/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
